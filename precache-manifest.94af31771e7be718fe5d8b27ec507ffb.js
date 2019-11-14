c5c03b8c8cc08f2d1a522c27b46f830a54e30a8d
RShtiltshen <f15her.ny@gmail.com>
2019-11-14 18:03:53 +0200

    Updates

---
+++ precache-manifest.94af31771e7be718fe5d8b27ec507ffb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b323371ddaf31208ecc284590b12927",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "852618233a2e1785b490",
+    "url": "/task-manager/static/css/main.01224f6a.chunk.css"
+  },
+  {
+    "revision": "fbb378b35d8e3ef82052",
+    "url": "/task-manager/static/js/2.a3ba905d.chunk.js"
+  },
+  {
+    "revision": "852618233a2e1785b490",
+    "url": "/task-manager/static/js/main.47f4fce5.chunk.js"
+  },
+  {
+    "revision": "6d02f0588f4c093ebd0a",
+    "url": "/task-manager/static/js/runtime-main.f55edcae.js"
+  }
+]);
